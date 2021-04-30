3375237350fd04a124ba6096972063f1af10cdaa
warrior21st <123767168@qq.com>
2021-05-01 00:22:51 +0800

    1

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-alert('it works.');
+var xhr=new XMLHttpRequest();
+xhr.onreadystatechange=function(){
+    if (xhr.readyState==4){
+        if (xhr.status==200)
+        {
+            var arr=JSON.parse(xhr.responseText);
+            var html="<ul>";
+            for(var i in arr){
+                var text=arr[i]["name"];
+                var url=arr[i]["html_url"];
+
+                html+="<li><a href='"+url+"' target='_blank'>"+text+"</a><li>";
+            }
+            html+="</ul>";
+
+            document.getElementById("blogs").innerHTML=html;
+        }
+        else
+        {
+            alert("Problem retrieving XML data");
+        }
+    }
+};
+xhr.open("GET","https://api.github.com/repos/warrior21st/blog/contents",false);
+xhr.send(null);
